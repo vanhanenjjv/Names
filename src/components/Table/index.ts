826f6c82c295b9b99cf0928287c475a94ca10641
Janne Vanhanen <janne.vanhanen@outlook.com>
2021-01-10 22:04:39 +0200

    Remove Material UI

--- src/components/Table/index.ts
+++
@@ -1,7 +0,0 @@
-import DataTable from './Data';
-import SkeletonTable from './Skeleton';
-import TableDefinition, { Column } from './table-definition';
-
-
-export { DataTable, SkeletonTable };
-export type { TableDefinition, Column };
