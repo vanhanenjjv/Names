1720d6fa2dfb87d2e830318e13f9aa0f74c29188
Janne Vanhanen <janne.vanhanen@outlook.com>
2021-01-17 22:34:34 +0200

    Center layout content

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { Names } from './pages';
 const App: React.FC = () => {
   return (
     <Layout style={{ height: '100%' }}>
-      <Layout.Content style={{ maxWidth: 960, margin: 16, justifyContent: 'center' }}>
+      <Layout.Content style={{ display: 'grid', maxWidth: 960, margin: 16, justifyContent: 'center' }}>
         <Names />
       </Layout.Content>
     </Layout>
